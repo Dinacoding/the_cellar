47e5a5d17776d0104e5b6ded9a1cbc77ff5ad2f8
dinacoding <dinarte.orepolo@gmail.com>
2025-08-19 22:40:50 +0100

    Complete the main header of the project

--- home/urls.py
+++ home/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home import views
+from django.urls import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-   path('', views.index, name='home'),
-]
+    path('', views.index, name='home'),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', include('home.urls')),  # Include the home app URLs
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
