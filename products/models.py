a443269bed102ce641d307995caaf95e1cfea531
dinacoding <dinarte.orepolo@gmail.com>
2025-08-21 12:19:04 +0100

    Create a url for demostrastion

--- products/models.py
+++ products/models.py
@@ -15,7 +15,7 @@ class Wine(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(blank=True, null=True)
-    
+
 
     def __str__(self):
         return self.name
