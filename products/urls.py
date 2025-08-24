7199b188f8f88f858985c0307a0136eff9fd29c6
dinacoding <dinarte.orepolo@gmail.com>
2025-08-25 00:38:24 +0100

    Create a query view

--- products/urls.py
+++ products/urls.py
@@ -8,6 +8,7 @@ def home_view(request):
     return HttpResponse("Home page is working!")
 
 urlpatterns = [
-    path('', views.all_wines, name='wines'),  
-    path('all_categories/', views.all_categories, name='categories'),
-    path('<int:wine_id>/', views.wine_detail, name='wine_detail'),]
+    path('', views.all_wines, name='all_wines'),  
+    path('all_categories/', views.all_categories, name='all_categories'),
+    path('<int:wine_id>/', views.wine_detail, name='wine_detail'),
+]
--- products/views.py
+++ products/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render, get_object_or_404
-from products.models import Wine, Category
+from django.shortcuts import render,redirect,reverse, get_object_or_404
+from products.models import Wine
+from django.contrib import messages
+from django.db.models import Q
 
 
 # Create your views here.
 def all_wines(request):
     """Render the wine index page."""
     wines = Wine.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('all_wine'))
+            wines = wines.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
     context = {       #create a context dictionary to pass data to the template
         'wines': wines,
+        'search_term': query,
     }
     return render(request, 'products.html', context)
 
