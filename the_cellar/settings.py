764c2a80a8fdff2ebd1be0cea77674e64a28146d
dinacoding <dinarte.orepolo@gmail.com>
2025-10-07 04:19:09 +0100

    Send it to the email

--- the_cellar/settings.py
+++ the_cellar/settings.py
@@ -115,7 +115,14 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_TIMEOUT = 5  
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  
 
 # settings.py - Allauth Configuration
 
