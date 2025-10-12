af6fee709d8b619ae4c2c74690aaeaeea2437f67
dinacoding <dinarte.orepolo@gmail.com>
2025-10-12 20:38:48 +0100

    Remove the wine_data.json

--- home/urls.py
+++ home/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
     path('shop/', include('shop.urls')),
-    
+    path('checkout/', include('checkout.urls')),
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
