3fd287464d78c7b491a7a7386d534d4519f27771
dinacoding <dinarte.orepolo@gmail.com>
2025-10-12 20:31:02 +0100

    Change email backend

--- the_cellar/settings.py
+++ the_cellar/settings.py
@@ -136,7 +136,7 @@ AUTHENTICATION_BACKENDS = [
 SITE_ID = 1
 
 # Email Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool) 
